f636b3ca3564606c86294123c459e4a6b58cdb1f
DavidBalazic <davidbalazic31@hotmail.com>
2025-05-31 16:48:34 +0200

    Sledenje eksperimentov

--- src/model/train.py
+++ src/model/train.py
@@ -6,6 +6,10 @@ import yaml
 import numpy as np
 import pandas as pd
 import tensorflow as tf
+import mlflow
+import dagshub
+import tf2onnx
+
 from lxml import etree as ET
 from sklearn.compose import ColumnTransformer
 from sklearn.metrics import mean_squared_error, mean_absolute_error
@@ -27,6 +31,9 @@ window_size = params["window_size"]
 target_col = params["target_col"]
 random_state = params["random_state"]
 
+# MLFlow init
+dagshub.init(repo_owner='DavidBalazic', repo_name='IIS_Vaje', mlflow=True)
+
 # Set PYTHONHASHSEED environment variable for reproducibility
 os.environ["PYTHONHASHSEED"] = str(random_state)
 
@@ -46,92 +53,130 @@ def build_model(input_shape):
     model.compile(optimizer="adam", loss="mean_squared_error")
     return model
 
+
+# Start MLflow experiment
+mlflow.set_experiment("LSTM_Air_Quality_Forecasting")
 for station in stations:
     print(f"\n--- Processing station {station} ---")
+    
+    with mlflow.start_run(run_name=f"{station}_run"):
+        mlflow.set_tag("station", station)
+        mlflow.log_param("window_size", window_size)
+        mlflow.log_param("test_size", test_size)
+        mlflow.log_param("random_state", random_state)
 
-    # Load the preprocessed data
-    df = pd.read_csv(f"data/preprocessed/air/{station}.csv")
-    df = df[["Date_to", target_col]]
-    
-    # Fill missing timestamps
-    date_preprocessor = DatePreprocessor("Date_to")
-    df = date_preprocessor.fit_transform(df)
-    df = df.drop(columns=["Date_to"], axis=1)
-    
-    # Use test_size hours of data for testing and the rest for training
-    df_test = df.iloc[-test_size:]
-    df_train = df.iloc[:-test_size]
-    
-    # Preprocess the numeric features
-    numeric_transformer = Pipeline([
-        ("fillna", SimpleImputer(strategy="mean")),
-        ("normalize", MinMaxScaler())
-    ])
-    
-    preprocess = ColumnTransformer([
-        ("numeric_transformer", numeric_transformer, [target_col]),
-    ])
-    
-    # Add the sliding window transformer to the pipeline
-    sliding_window_transformer = SlidingWindowTransformer(window_size)
-    
-    # Create the pipeline
-    pipeline = Pipeline([
-        ("preprocess", preprocess),
-        ("sliding_window_transformer", sliding_window_transformer),
-    ])
-    
-    # Apply the pipeline to the dataframe
-    X_train, y_train = pipeline.fit_transform(df_train)
-    X_test, y_test = pipeline.transform(df_test)
-    
-    print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
-    print(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
-    
-    # Build and train tensorflow keras model
-    input_shape = (X_train.shape[1], X_train.shape[2])
-    model = build_model(input_shape)
-    
-    # Train the model
-    early_stopping = EarlyStopping(monitor="val_loss", patience=5, restore_best_weights=True)
-    history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
-    
-    # Invert the scaling
-    y_train = y_train.reshape(-1, 1)
-    y_test = y_test.reshape(-1, 1)
-    
-    y_train = pipeline.named_steps["preprocess"].transformers_[0][1].transform(y_train)
-    y_test = pipeline.named_steps["preprocess"].transformers_[0][1].transform(y_test)
-    
-    # Evaluate the model
-    y_pred = model.predict(X_test)
-    mse = mean_squared_error(y_test, y_pred)
-    mae = mean_absolute_error(y_test, y_pred)
-    print(f"Test MAE: {mae}")
-    print(f"Test MSE: {mse}")
-    print(f"Test RMSE: {np.sqrt(mse)}")
-    
-    # Train the model on the entire dataset
-    X_full, y_full = pipeline.fit_transform(df)
-    model = build_model((X_full.shape[1], X_full.shape[2]))
-    model.fit(X_full, y_full, epochs=50, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
-    
-    # Invert the scaling
-    y_full = y_full.reshape(-1, 1)
-    y_full = pipeline.named_steps["preprocess"].transformers_[0][1].transform(y_full)
-    X_full = X_full.reshape(X_full.shape[0], X_full.shape[1])
-    
-    # Evaluate the model on the entire dataset
-    y_pred_full = model.predict(X_full)
-    mse_full = mean_squared_error(y_full, y_pred_full)
-    mae_full = mean_absolute_error(y_full, y_pred_full)
-    print(f"Full dataset MAE: {mae_full}")
-    print(f"Full dataset MSE: {mse_full}")
-    print(f"Full dataset RMSE: {np.sqrt(mse_full)}")
-    
-    # Save the model
-    os.makedirs("models", exist_ok=True)
-    model.save(f"models/model_{station}.keras")
-    
-    # Save the pipeline
-    joblib.dump(pipeline, f"models/pipeline_{station}.pkl")
+        # Load the preprocessed data
+        df = pd.read_csv(f"data/preprocessed/air/{station}.csv")
+        df = df[["Date_to", target_col]]
+
+        # Fill missing timestamps
+        date_preprocessor = DatePreprocessor("Date_to")
+        df = date_preprocessor.fit_transform(df)
+        df = df.drop(columns=["Date_to"], axis=1)
+
+        # Use test_size hours of data for testing and the rest for training
+        df_test = df.iloc[-test_size:]
+        df_train = df.iloc[:-test_size]
+
+        # Preprocess the numeric features
+        numeric_transformer = Pipeline([
+            ("fillna", SimpleImputer(strategy="mean")),
+            ("normalize", MinMaxScaler())
+        ])
+
+        preprocess = ColumnTransformer([
+            ("numeric_transformer", numeric_transformer, [target_col]),
+        ])
+
+        # Add the sliding window transformer to the pipeline
+        sliding_window_transformer = SlidingWindowTransformer(window_size)
+
+        # Create the pipeline
+        pipeline = Pipeline([
+            ("preprocess", preprocess),
+            ("sliding_window_transformer", sliding_window_transformer),
+        ])
+
+        # Apply the pipeline to the dataframe
+        X_train, y_train = pipeline.fit_transform(df_train)
+        X_test, y_test = pipeline.transform(df_test)
+
+        print(f"X_train shape: {X_train.shape}, y_train shape: {y_train.shape}")
+        print(f"X_test shape: {X_test.shape}, y_test shape: {y_test.shape}")
+
+        # Build and train tensorflow keras model
+        input_shape = (X_train.shape[1], X_train.shape[2])
+        model = build_model(input_shape)
+        
+        # Enable MLflow autologging for TensorFlow
+        mlflow.tensorflow.autolog()
+
+        # Train the model
+        early_stopping = EarlyStopping(monitor="val_loss", patience=5, restore_best_weights=True)
+        history = model.fit(X_train, y_train, epochs=50, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+        
+        # Train the model
+        y_pred = model.predict(X_test)
+
+        # Invert the scaling
+        scaler = pipeline.named_steps["preprocess"].transformers_[0][1].named_steps["normalize"]
+        y_pred_inverted = scaler.inverse_transform(y_pred.reshape(-1, 1))
+        y_test_inverted = scaler.inverse_transform(y_test.reshape(-1, 1))
+
+        # Evaluate the model
+        mse = mean_squared_error(y_test_inverted, y_pred_inverted)
+        mae = mean_absolute_error(y_test_inverted, y_pred_inverted)
+        rmse = np.sqrt(mse)
+        print(f"Test MAE: {mae}")
+        print(f"Test MSE: {mse}")
+        print(f"Test RMSE: {rmse}")
+
+        # Log metrics
+        mlflow.log_metric("test_mae", mae)
+        mlflow.log_metric("test_mse", mse)
+        mlflow.log_metric("test_rmse", rmse)
+
+        # Train the model on the entire dataset
+        X_full, y_full = pipeline.fit_transform(df)
+        model = build_model((X_full.shape[1], X_full.shape[2]))
+        model.fit(X_full, y_full, epochs=50, batch_size=32, validation_split=0.2, callbacks=[early_stopping])
+        
+        # Train the model on the full dataset
+        y_pred_full = model.predict(X_full)
+
+        # Invert the scaling
+        scaler = pipeline.named_steps["preprocess"].transformers_[0][1].named_steps["normalize"]
+        y_pred_full_inverted = scaler.inverse_transform(y_pred_full.reshape(-1, 1))
+        y_full_inverted = scaler.inverse_transform(y_full.reshape(-1, 1))
+
+        # Evaluate the model on the entire dataset
+        mse_full = mean_squared_error(y_full_inverted, y_pred_full_inverted)
+        mae_full = mean_absolute_error(y_full_inverted, y_pred_full_inverted)
+        rmse_full = np.sqrt(mse_full)
+        print(f"Full dataset MAE: {mae_full}")
+        print(f"Full dataset MSE: {mse_full}")
+        print(f"Full dataset RMSE: {rmse_full}")
+
+        # Log full dataset metrics
+        mlflow.log_metric("full_mae", mae_full)
+        mlflow.log_metric("full_mse", mse_full)
+        mlflow.log_metric("full_rmse", rmse_full)
+
+        # Save the model
+        os.makedirs("models", exist_ok=True)
+        model.save(f"models/model_{station}.keras")
+        mlflow.keras.log_model(model, artifact_path=f"model_{station}")
+        
+        # Convert Keras model to ONNX and log it in MLflow
+        input_signature = [tf.TensorSpec([None, window_size, 1], tf.float32, name="input")]
+        onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature=input_signature, opset=13)
+        mlflow.onnx.log_model(onnx_model, artifact_path=f"onnx_model_{station}")
+
+        # Save the pipeline
+        joblib.dump(pipeline, f"models/pipeline_{station}.pkl")
+        mlflow.log_artifact(f"models/pipeline_{station}.pkl")
+
+        # Register the model in MLflow Model Registry
+        onnx_model_uri = f"runs:/{mlflow.active_run().info.run_id}/onnx_model_{station}"
+        registered_model_name = f"LSTM_ONNX_Model_{station}"
+        mlflow.register_model(model_uri=onnx_model_uri, name=registered_model_name)
